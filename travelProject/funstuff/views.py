0f28c5d60cd7f26ed0dca77c64c3bd42e451c2c9
bowser <g122258632@gmail.com>
2025-03-14 04:03:45 +0800

    更新爆炸表單功能，新增延遲顯示字母及自訂爆炸文字 修正位置置中

--- travelProject/funstuff/views.py
+++ travelProject/funstuff/views.py
@@ -18,7 +18,7 @@ def feedback_explosive_view_PHYSICS(request):
     # example: check if request.method == 'POST': ...
     
     # 爆炸文字可自訂，也可來自 DB / form 
-    explosion_text = "PHYSICS!"
+    explosion_text = "設計者:bow8234"
     
     context = {
         'user': request.user,        # 假設你想在模板顯示使用者
