5cdab5c27d0c9b86a67d3b7f3d6a98eef87620ff
Lynn Chen <jalynnchen@gmail.com>
2025-03-18 15:11:56 +0800

    bookkeeping第二版

--- travelProject/bookkeeping/models.py
+++ travelProject/bookkeeping/models.py
@@ -15,7 +15,7 @@ class Record(models.Model):
         ]
     types = models.CharField(max_length=20,choices=type_choiches,default='food')
     amount = models.DecimalField(max_digits=10,decimal_places=2)
-    note = models.CharField(max_length=255)
+    note = models.CharField(max_length=255,blank=True)
     
 class MemberRelation(models.Model):
     no_id = models.IntegerField()
